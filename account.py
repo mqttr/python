6b9e6d2b37d2547fd55caab62c65a4e90888f5e3
Robogamer1029 <matt.roland1029@gmail.com>
2023-08-08 17:09:08 -0500

    Update account.py

--- account.py
+++ account.py
@@ -1,20 +1,37 @@
 class Account:
-    def __init__(self, name):
-        self.__account_name = name
-        self.__account_balance = 0
-    
-    def deposit(self, amount):
-        self.__account_balance += amount
+    def __init__(self, name: str) -> None:
+        '''
+        Creates an account with a zero (0) balance
 
-    def withdraw(self, amount):
+        :param name: Sets the account name
+        '''
+        self.__account_name = name
+        self.__account_balance = 0.0
+    
+    def deposit(self, amount: float) -> bool:
+        '''
+        Incriments account balance fails when new balance is less than old
+        :param amount: Change of account balance
+        '''
+
+        newAmount = self.__account_balance + amount
+        if newAmount <= self.__account_balance:
+            return False
+        return True
+
+    def withdraw(self, amount: float) -> bool:
+        '''
+        Decrements account balance fails when new balance is more than old
+        :param amount: Change of account balance
+        '''
         newAmount = self.__account_balance - amount
-        if newAmount < 0:
+        if newAmount < 0 or newAmount >= self.__account_balance:
             return False
 
         self.__account_balance = newAmount
         return True
 
-    def get_balance(self):
+    def get_balance(self) -> float:
         return self.__account_balance
-    def get_name(self):
+    def get_name(self) -> str:
         return self.__account_name
