1f90dd760048e00e7684870b6ba9523dd43e29a1
Robogamer1029 <matt.roland1029@gmail.com>
2023-08-09 16:24:56 -0500

    Part 1

--- account.py
+++ account.py
@@ -3,6 +3,7 @@ class Account:
         self.__account_name = name
         self.__account_balance = 0.0
     
+<<<<<<< Updated upstream
     def deposit(self, amount: float):
         newAmount = self.__account_balance + amount
         if newAmount <= self.__account_balance:
@@ -12,12 +13,27 @@ class Account:
     def withdraw(self, amount: float):
         newAmount = self.__account_balance - amount
         if newAmount < 0 or newAmount >= self.__account_balance:
+=======
+    def deposit(self, amount):
+        newAmount = self.__account_balance + amount
+        if newAmount <= self.__account_balance:
+>>>>>>> Stashed changes
             return False
-
         self.__account_balance = newAmount
         return True
 
-    def get_balance(self):
+    def withdraw(self, amount) :
+        newAmount = self.__account_balance - amount
+        if newAmount < 0 or newAmount >= self.__account_balance:
+            return False
+        self.__account_balance = newAmount
+        return True
+
+    def get_balance(self) -> float:
         return self.__account_balance
+<<<<<<< Updated upstream
     def get_name(self):
+=======
+    def get_name(self) -> str:
+>>>>>>> Stashed changes
         return self.__account_name
