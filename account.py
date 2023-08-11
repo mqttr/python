67e95dce94a35b32eb34939e68bd7ab9502cdad5
Matthew <matthew.roland56@gmail.com>
2023-08-11 16:59:41 -0500

    corrected feedback

--- account.py
+++ account.py
@@ -3,36 +3,49 @@ class Account:
         '''
         Creates an account with a zero (0) balance
 
-        :param name: Sets the account name
+        :param name: Account's user's name
         '''
         self.__account_name = name
         self.__account_balance = 0.0
     
     def deposit(self, amount: float) -> bool:
-        newAmount = self.__account_balance + amount
-        if newAmount <= self.__account_balance:
+        '''
+        Method decrements account balance. It fails when new balance is less than previous balance or amount is <= 0.
+
+        :param amount: Increments balance by amount
+        :return: True/False depending on success of withdraw
+        '''
+        newBalance = self.__account_balance + amount
+        if newBalance <= self.__account_balance:
             return False
-        self.__account_balance = newAmount
+        self.__account_balance = newBalance
         return True
 
     def withdraw(self, amount: float) -> bool:
         '''
-        Decrements account balance fails when new balance is more than old
-        :param amount: Change of account balance
+        Method increments account balance. It fails when new balance is more than previous balance or amount is <= 0.
+
+        :param amount: Decrements balance by amount
+        :return: True/False depending on success of withdraw
         '''
-        newAmount = self.__account_balance - amount
-        if newAmount < 0 or newAmount >= self.__account_balance:
+        newBalance = self.__account_balance - amount
+        if newBalance < 0 or newBalance >= self.__account_balance:
             return False
-        self.__account_balance = newAmount
+        self.__account_balance = newBalance
         return True
 
     def get_balance(self) -> float:
         '''
-        Returns the current balance for the object
+        Method gets the current balance of the account
+
+        :return: Account User's current balance
         '''
         return self.__account_balance
+
     def get_name(self) -> str:
         '''
-        Returns the name for the object
+        Method gets the user's name of the account
+
+        :return: Account User's full name
         '''
         return self.__account_name
--- test_account.py
+++ test_account.py
@@ -2,66 +2,132 @@ from account import *
 import pytest
 
 class TestAccount():
+    def test_init(self):
+        for accountName in [ "TEST_ACCOUNT", "John" ]:
+            t = Account(accountName)
+            assert pytest.approx(0) == t.get_balance()
+            assert accountName is t.get_name()
+
     def test_deposit(self):
         t = Account("TEST_ACCOUNT")
         
+        # Bad amounts
         assert False == t.deposit(-1)
-        assert False == t.deposit(0)
+        assert pytest.approx(0) == t.get_balance()
 
+        assert False == t.deposit(0)
+        assert pytest.approx(0) == t.get_balance()
+
+        # Int
         assert True == t.deposit(10)
-        assert True == t.deposit(.5)
-        
-        assert False == t.deposit(-1)
-        assert False == t.deposit(0)
+        assert pytest.approx(10) == t.get_balance()
 
-        with pytest.raises( TypeError ):
-            t.deposit("H")
-            t.deposit(None)
+        # Float
+        assert True == t.deposit(.5)
+        assert pytest.approx(10.5) == t.get_balance()
+
+        # Bad amounts w/ non-zero balance
+        assert False == t.deposit(-1)
+        assert pytest.approx(10.5) == t.get_balance()
+        
+        assert False == t.deposit(0)
+        assert pytest.approx(10.5) == t.get_balance()
         
     def test_withdraw(self):
         t = Account("TEST_ACCOUNT")
 
+        # large withdraw w/ 0 balance
         assert False == t.withdraw(1)
-        assert False == t.withdraw(-1)
+        assert pytest.approx(0) == t.get_balance()
 
-        t.deposit(999)
+        # Negative withdraw
         assert False == t.withdraw(-1)
+        assert pytest.approx(0) == t.get_balance()
+
+        # Zero withdraw
         assert False == t.withdraw(0)
+        assert pytest.approx(0) == t.get_balance()
 
+        t.deposit(10)
+        # Negative withdraw w/ non-zero balance
+        assert False == t.withdraw(-1)
+        assert pytest.approx(10) == t.get_balance()
+
+        # Zero withdraw w/ non-zero balance
+        assert False == t.withdraw(0)
+        assert pytest.approx(10) == t.get_balance()
+
+        # Int withdraw
         assert True == t.withdraw(1)
-        assert True == t.withdraw(1.5)
+        assert pytest.approx(9) == t.get_balance()
 
-        t.withdraw(.5)
-        assert True == t.withdraw(996.0)
+        # Float withdraw
+        assert True == t.withdraw(1.5)
+        assert pytest.approx(7.5) == t.get_balance()
+
+        # Large float withdraw
+        assert False == t.withdraw(10.5)
+        assert pytest.approx(7.5) == t.get_balance()
+
+        # Large int withdraw
+        assert False == t.withdraw(10)
+        assert pytest.approx(7.5) == t.get_balance()
+        
+        # Back to 0 withdraws
+        assert True == t.withdraw(.5)
+        assert True == t.withdraw(7.0)
         assert pytest.approx(0) == t.get_balance()
         
-        t.deposit(999)
-
-        with pytest.raises( TypeError ):
-            t.withdraw("H")
-            t.withdraw(None)
-
 
     def test_get_balance(self):
         t = Account("TEST_ACCOUNT")
 
-        assert pytest.approx(0) == t.get_balance()
-        t.withdraw(1)
-        assert pytest.approx(0) == t.get_balance()
-        t.deposit(5.5)
-        assert pytest.approx(5.5) == t.get_balance()
-        t.deposit(5)
-        assert pytest.approx(10.5) == t.get_balance()
-        t.withdraw(11)
-        assert pytest.approx(10.5) == t.get_balance()
-        t.withdraw(10.5)
+        # Initial bal
         assert pytest.approx(0) == t.get_balance()
 
-        t.deposit(5)
-        t.deposit(-5)
+        # Float deposit
+        assert True == t.deposit(5.5)
+        assert pytest.approx(5.5) == t.get_balance()
+
+        # Int deposit
+        assert True == t.deposit(5)
+        assert pytest.approx(10.5) == t.get_balance()
+
+        # Int withdraw
+        assert True == t.withdraw(1)
+        assert pytest.approx(9.5) == t.get_balance()
+
+        # Float withdraw
+        assert True == t.withdraw(1.0)
+        assert pytest.approx(8.5) == t.get_balance()
+
+        # Float large withdraw
+        assert False == t.withdraw(99.99)
+        assert pytest.approx(8.5) == t.get_balance()
+
+        # int large withdraw
+        assert False == t.withdraw(11)
+        assert pytest.approx(8.5) == t.get_balance()
+
+        # Zeroing
+        assert True == t.withdraw(8.5)
+        assert pytest.approx(0) == t.get_balance()
+
+        # Non-zeroing
+        assert True == t.deposit(5)
         assert pytest.approx(5) == t.get_balance()
-        t.withdraw(-5)
-        assert pytest.approx(5) == t.get_balance()        
+
+        # Negative amount w/ non-zero balance
+        assert False == t.deposit(-5)
+        assert pytest.approx(5) == t.get_balance()
+        assert False == t.withdraw(-5)
+        assert pytest.approx(5) == t.get_balance()
+
+        # zero amount w/ non-zero balance
+        assert False == t.deposit(0)
+        assert pytest.approx(5) == t.get_balance()
+        assert False == t.withdraw(0)
+        assert pytest.approx(5) == t.get_balance() 
 
     def test_get_name(self, *args):
         for nameTest in args:
@@ -72,7 +138,8 @@ class TestAccount():
 if __name__ == "__main__":
     tester = TestAccount()
 
-    tester.test_get_name("t", "John Oliver", "", "5", 3.5, 1, None)
+    tester.test_init()
+    tester.test_get_name("t", "John Oliver", "Ted Teddybear")
     tester.test_get_balance()
     tester.test_withdraw()
     tester.test_deposit()
